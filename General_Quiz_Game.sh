70fb979314006514bb94d54d753e9a78f38862d8
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2026-01-22 15:21:29 -0600

    Finished my first quiz

--- General_Quiz_Game.sh
+++ General_Quiz_Game.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 
+
+
+geography_quiz(){
+	# Array of geography questions
+geography_questions=(
+	"1. What is the difference between weather and climate?"
+	"2. What type of plate boundary causes earthquakes and volcanoes?"
+	"3. Which line divides the Earth into the Northern and Southern Hemispheres?"
+	"4. What is urbanization?"
+	"5. Which continent has the largest population?"
+)
+
+# Array of options for each question
+geography_options=(
+	"a) Short-term vs long-term | b) Rain vs snow | c) Hot vs cold | d) Wind vs humidity"
+	"a) Divergent | b) Convergent | c) Transform | d) Subduction"
+	"a) Tropic of Cancer | b) Etubinquator | c) Prime Meridian | d) Arctic Circle"
+	"a) City expansion | b) Farming | c) Deforestation | d) Pollution"
+	"a) Africa | b) Europe | c) Asia | d) North America"
+)
+
+geography_answers=("a" "b" "b" "a" "c")
+
+question=0
+for q in "${geography_questions[@]}"; do
+	echo "$q"
+	echo "${geography_options[$question]}"
+	read -p "Answer: " answerg
+	answer="${geography_answers[$question]}"
+		if [ "$answerg" == "$answer" ];then
+				echo "You got it right!!"
+		else
+				echo "You got it wrong!! The correct answer is: $answer"
+		fi
+		((question++))
+done
+
+}
+
 display_menu_options(){
 	# This shows all the choices you can pick from and asks you to type one.
-	echo -e "
+	echo -e " Pick an option below:
 	1. Geography Quiz
 	2. Math Quiz
 	3. Science Quiz
@@ -12,35 +51,35 @@ display_menu_options(){
 	7. Exit
 	"
 	read -p "Choose an option (1-7): " function_choice
-	check_user_input
 }
-
+display_menu_options
 check_user_input(){
 	# This looks at what number you typed and runs the matching action.
 	case  $function_choice in
-		1)
-			Geography_Quiz
-			;;
-		2)
-			Math_Quiz
-			;;
-		3)
-			Science_Quiz
-			;;
-		4)
-			Basketball_Quiz
-			;;
-		5)
-			Soccer_Quiz
-			;;
-		6)
-			General_Quiz
-			;;
-		7)
-			end_script   # Stops the program.
-			;;
-		*)
-			display_menu_options   # If you typed a wrong number, it shows the menu again.
-			;;
+	1)
+		geography_quiz
+		;;
+	2)
+		math_quiz
+		;;
+	3)
+		science_quiz
+		;;
+	4)
+		basketball_quiz
+		;;
+	5)
+		soccer_quiz
+		;;
+	6)
+		general_quiz
+		;;
+	7)
+		end_script   # Stops the program.
+		;;
+	*)
+		display_menu_options   # If you typed a wrong number, it shows the menu again.
+		;;
 	esac	
 }
+check_user_input
---
+++ Quiz.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+display_menu_options(){
+	case display_menu_options in 
+	1) 
+		Pick an option below:
+		;;
+	2) Geography Quiz
+		;;
+	3) Math Quiz
+		;;
+	4) Science Quiz
+	5. Basketball Quiz
+	6. Soccer Quiz
+	7. General Quiz
+	8. Exit
+	
+	read -p "Choose an option (1-7): " function_choice
+}"
+
+	
+}
+display_menu_options
